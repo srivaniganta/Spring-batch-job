20237269760eecee398774a74ca26479e769e16e
venkat ganta <venkatganta@venkats-MacBook-Pro.local>
2021-05-26 15:40:11 -0700

    batch first commit

---
+++ src/main/java/com/example/Springbatchjob/SpringBatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbatchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springbatchjob/SpringBatchJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbatchjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
