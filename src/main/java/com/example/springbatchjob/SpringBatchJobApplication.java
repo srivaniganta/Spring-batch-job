791f3c5f8b51d8174c20e6b5e50062e9185509cf
venkat ganta <venkatganta@venkats-MacBook-Pro.local>
2021-05-26 22:51:26 -0700

    batch commit

--- src/main/java/com/example/springbatchjob/SpringBatchJobApplication.java
+++ src/main/java/com/example/springbatchjob/SpringBatchJobApplication.java
@@ -1,9 +1,11 @@
-package com.example.Springbatchjob;
+package com.example.springbatchjob;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchJobApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/springbatchjob/SpringBatchJobApplicationTests.java
+++ src/test/java/com/example/springbatchjob/SpringBatchJobApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Springbatchjob;
+package com.example.springbatchjob;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
